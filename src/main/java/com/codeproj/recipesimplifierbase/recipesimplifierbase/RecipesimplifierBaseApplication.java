4c90c570f21b2c0ead75ebb9621437b758399e6c
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2017-12-13 19:51:35 +0100

    Initial commit with spring boot skeleton

---
+++ src/main/java/com/codeproj/recipesimplifierbase/recipesimplifierbase/RecipesimplifierBaseApplication.java
@@ -0,0 +1,12 @@
+package com.codeproj.recipesimplifierbase.recipesimplifierbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipesimplifierBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipesimplifierBaseApplication.class, args);
+	}
+}
