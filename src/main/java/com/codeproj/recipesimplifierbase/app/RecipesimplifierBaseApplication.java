686172ee526bae6ad7d547691dc25dc94d27f2e0
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2017-12-16 15:48:48 +0100

    renames base package

--- src/main/java/com/codeproj/recipesimplifierbase/app/RecipesimplifierBaseApplication.java
+++ src/main/java/com/codeproj/recipesimplifierbase/app/RecipesimplifierBaseApplication.java
@@ -1,4 +1,4 @@
-package com.codeproj.recipesimplifierbase.run;
+package com.codeproj.recipesimplifierbase.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
