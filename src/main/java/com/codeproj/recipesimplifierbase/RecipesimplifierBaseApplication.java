3fb779956470fac7ee294d0a00887ba5ead06369
Aron Harsfalvi <aron.harsfalvi@gmail.com>
2017-12-27 16:51:19 +0100

    Adds CORS rules for localhost:4200

--- src/main/java/com/codeproj/recipesimplifierbase/RecipesimplifierBaseApplication.java
+++ src/main/java/com/codeproj/recipesimplifierbase/RecipesimplifierBaseApplication.java
@@ -2,6 +2,10 @@ package com.codeproj.recipesimplifierbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class RecipesimplifierBaseApplication {
@@ -9,4 +13,17 @@ public class RecipesimplifierBaseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(RecipesimplifierBaseApplication.class, args);
 	}
+	
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+            }
+        };
+    }
 }
+
+
+
--- src/main/java/com/codeproj/recipesimplifierbase/auth/TokenAuthenticationFilter.java
+++ src/main/java/com/codeproj/recipesimplifierbase/auth/TokenAuthenticationFilter.java
@@ -54,5 +54,5 @@ public class TokenAuthenticationFilter extends OncePerRequestFilter {
         
         chain.doFilter(request, response);
     }
-
+    
 }
